a7f59ba613ed031f2823315930b79b19d2a907b0
Patrick Thoelken <patrick.thoelken@outlook.com>
2022-03-30 22:09:00 +0200

    update functions, update readme

--- README.md
+++ README.md
@@ -2,15 +2,21 @@
 Update your Cloudflare DNS Records for e.g. DynDNS
 
 # How to use it
-
 First of all, you have to register a domain at GoDaddy for example and set the nameservers to the Cloudflare nameservers. After this, you go ahead to Cloudflare and check in your domain as free and wait until Cloudflare tells you all is up and fine. 
 
 1. Get your Cloudflare API Key https://dash.cloudflare.com/profile/api-tokens
 2. Create a real domain zone and domain record with fake values (e.g. 8.8.8.8, non proxified) just for creation
-2. Configure the script variables section for your usecase
-3. Run this script manually or automatically by a cronjob
+3. Clone this repo to your linux server / RaspberryPi
+4. sudo chmod +x cloudflare-dyndns-updater.sh
+5. Configure the script variables section for your usecase
+6. Run this script manually or automatically by a cronjob
 
-For example your running this from your home network at a Raspberry Pi or Linux Computer you don't have to configure dyndns settings in your home router. You have to just configure your open ports for incoming traffic to your destination. 
+## Run usage Example
+* sudo ./cloudflare-dyndns-updater.sh --help
+* sudo ./cloudflare-dyndns-updater.sh --Cleanup
+* sudo ./cloudflare-dyndns-updater.sh --update
+
+Last one open the necessary ports from your router to the linux server / RaspberryPi which you want to reach from the internet.
 
 # Example build for home
 - cloud.foo.bar (Port 443, public domain, GoDaddy / Namecheap registrated) **->** Cloudflare integrated **-> ||| 🏠 here begins your home network 🏠 ||| ->** your home pi update the dns records from your public router ip to cloudflare for cloud.foo.bar **->** you have to configure in your home router, that you accept port 443 incoming to your cloud-server (virtual machine, another pi, ...)
--- cloudflare-dyndns-updater.sh
+++ cloudflare-dyndns-updater.sh
@@ -1,74 +1,123 @@
-#!/bin/bash
+#!/bin/bash -ex
 # Cloudflare as Dynamic DNS Updater
 
-### EXAMPLE CRONTAB SETTIGNS FOR 6 HOURS -> EDIT /etc/crontab ###
-0 */6 * * *     root    /bin/bash /opt/PTL/cloudflare/cloudflare-dyndns-updater.sh
-
-# Update these with real values
-auth_email="your email addr"
-auth_key="your api key"
+# ---> Update these variables with your relevants input
+auth_email="your email addr" # your cloudflare login email address
+auth_key="your api key" # your cloudflare general or specific api key from https://dash.cloudflare.com/profile/api-tokens
 zone_name="your main dns zone" # e.g. mydomain.com
 record_name="your specified dns record name" # e.g. subdomain.mydomain.com OR mydomain.com
-proxied=true # If you want to secure your home ip address and use Cloudflare proxy features
+proxied=true # If you want to secure your home ip address and use Cloudflare proxy features. Not recommended for vpn or other direct ip services. Recommended for HTTP/HTTPS services.
 ttl="1800" # Your domain time to live time
 
-# More Settings
+# ---> Do not edit variables from here
+tmpfolder=/tmp/cloudflare-dyndns-updater
 ip=$(curl -s https://ipv4.icanhazip.com)
-ip_file="ip.txt"
-id_file="cloudflare.ids"
-log_file="cloudflare.log"
+ip_file="$tmpfolder/cloudflare.ips"
+id_file="$tmpfolder/cloudflare.ids"
+log_file="$tmpfolder/cloudflare.log"
 
-# Keep files in the same folder when run from cron
-current="$(pwd)"
+if [ "$EUID" -ne 0 ]; then
+    echo -e "[$(date)] - $0 must be run as sudo. Application exit."
+    exit 1
+fi
+
+if [ ! -d $tmpfolder ]; then
+    mkdir -p $tmpfolder
+fi
+
 cd "$(dirname "$(readlink -f "$0")")"
 
-log() {
+ConsoleLog() {
     if [ "$1" ]; then
-        echo -e "[$(date)] - $1" >> $log_file
+        echo -e "[$(date)] - $1" | tee -a $log_file
     fi
 }
 
-log "Check Initiated"
-
-if [ -f $ip_file ]; then
-    old_ip=$(cat $ip_file)
-    if [ $ip == $old_ip ]; then
-        log "IP has not changed."
-        exit 0
+function CheckFileContent() {
+    if [ -f $1 ] && ( ! grep -q '[^[:space:]]' $1); then
+        ConsoleLog "$1 content is empty and will be deleted. Please re-run this script."
+        sudo rm -rf $1
+        exit 1
     fi
-fi
+}
 
-if [ -f $id_file ] && [ $(wc -l $id_file | cut -d " " -f 1) == 2 ]; then
-    zone_identifier=$(head -1 $id_file)
-    record_identifier=$(tail -1 $id_file)
-else
-    zone_identifier=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone_name" \
-    -H "X-Auth-Email: $auth_email" -H "X-Auth-Key: $auth_key" \
-    -H "Content-Type: application/json" | grep -Po '(?<="id":")[^"]*' | head -1 )
-    
-    record_identifier=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_name" \
+function updateIPToCloudflare() {
+    ConsoleLog "Check Initiated"
+
+    if [ -f $ip_file ]; then
+        old_ip=$(cat $ip_file)
+        if [ $ip == $old_ip ]; then
+            ConsoleLog "IP has not changed."
+            exit 0
+        fi
+    fi
+
+    if [ -f $id_file ] && [ $(wc -l $id_file | cut -d " " -f 1) == 2 ]; then
+        zone_identifier=$(head -1 $id_file)
+        record_identifier=$(tail -1 $id_file)
+    else
+
+        zone_identifier=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone_name" \
+        -H "X-Auth-Email: $auth_email" -H "X-Auth-Key: $auth_key" \
+        -H "Content-Type: application/json" | grep -Po '(?<="id":")[^"]*' | head -1 )
+
+        record_identifier=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_name" \
+        -H "X-Auth-Email: $auth_email" \
+        -H "X-Auth-Key: $auth_key" \
+        -H "Content-Type: application/json" | grep -Po '(?<="id":")[^"]*')
+
+        echo "$zone_identifier" > $id_file
+        echo "$record_identifier" >> $id_file
+    fi
+
+    update=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$record_identifier" \
     -H "X-Auth-Email: $auth_email" \
     -H "X-Auth-Key: $auth_key" \
-    -H "Content-Type: application/json" | grep -Po '(?<="id":")[^"]*')
-    
-    echo "$zone_identifier" > $id_file
-    echo "$record_identifier" >> $id_file
-fi
+    -H "Content-Type: application/json" \
+    --data "{\"id\":\"$zone_identifier\",\"type\":\"A\",\"name\":\"$record_name\",\"content\":\"$ip\",\"ttl\":\"$ttl\",\"proxied\":$proxied}")
 
-update=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records/$record_identifier" \
--H "X-Auth-Email: $auth_email" \
--H "X-Auth-Key: $auth_key" \
--H "Content-Type: application/json" \
---data "{\"id\":\"$zone_identifier\",\"type\":\"A\",\"name\":\"$record_name\",\"content\":\"$ip\",\"ttl\":\"$ttl\",\"proxied\":$proxied}")
+    if [[ $update == *"\"success\":false"* ]]; then
+        message="API UPDATE FAILED. DUMPING RESULTS:\n$update"
+        ConsoleLog "$message"
+        echo -e "$message"
+        exit 1 
+    else
+        echo "$ip" > $ip_file
+        ConsoleLog "Youre IP is changed and updated to: $ip"
+    fi
+}
 
-if [[ $update == *"\"success\":false"* ]]; then
-    message="API UPDATE FAILED. DUMPING RESULTS:\n$update"
-    log "$message"
-    echo -e "$message"
-    exit 1 
-else
-    message="IP changed to: $ip"
-    echo "$ip" > $ip_file
-    log "$message"
-    echo "$message"
-fi
+function helpManual() {
+    echo -e ""
+    echo -e "---------- USAGE OF $0 ----------"
+    echo -e ""
+    echo -e "$0 --update: Update your ip address to cloudflare and save results to $tmpfolder"
+    echo -e "$0 --cleanup: Cleanup the $tmpfolder Folder"
+    echo -e "$0 --help: Shows you the help manual."
+    echo -e ""
+}
+
+function housekeeping() {
+    rm -rf $tmpfolder/*.*
+    echo -e "[$(date)] - $tmpfolder cleanup complete."
+}
+
+case "$1" in
+        "--cleanup")
+        housekeeping
+        ;;
+        "--update")
+        CheckFileContent $id_file
+        updateIPToCloudflare
+        ;;
+        "--help")
+        helpManual
+        ;;
+        *)
+        echo -e ""
+        echo -e "Unknown argument for $0 please use { --cleanup | --update | --help }"
+        helpManual
+        exit 1
+        ;;
+esac
+exit 0
